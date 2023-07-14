065ac5598d8e3cfc2ebdcedf91fc9fd100782b29
CHAGOUR-Maryam <139329090+CHAGOUR-Maryam@users.noreply.github.com>
2023-07-14 14:11:56 +0100

    Update email_sender.py

--- email_sender.py
+++ email_sender.py
@@ -6,4 +6,5 @@ def email():
     st.markdown("Step 1: get your password from gmail:")
     st.markdown("""First: Turn on 2 step verification
             1. Login to your Gmail account. In the upper-right corner, select your Image. From the menu, click on Manage your Gmail account.""")
-    Image.open("pic1.png")
+    im=Image.open("pic1.png")
+    st.image(im)
